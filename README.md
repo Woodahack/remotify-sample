8d52721a13955b8ae23d01b3beccc7fec1a3b43a
Renzo Plaisant van der Wal <r.v.plaisantvanderwal@gmail.com>
2013-11-25 09:25:56 +0100

    added sample app files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-remotify-sample
-===============
+Remotify sample app
+===================
 
 This is a sample app for the Remotify plugin available for the Corona SDK
---
+++ config.lua
@@ -0,0 +1,13 @@
+application =
+{
+	content =
+	{
+		width = 320,
+		height = 480,
+		scale = "letterbox",
+		-- graphicsCompatibility = 1,   --- This turns on V1 Compatibility mode
+		-- scale = "none", 
+		-- scale = "letterbox",
+		-- scale = "zoomStretch", 
+	},
+} 
